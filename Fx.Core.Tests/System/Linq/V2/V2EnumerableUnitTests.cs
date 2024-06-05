8e2a3b20fabc2709aab5b7d5ef3193a9a42d908f
ravudet <170215607+ravudet@users.noreply.github.com>
2024-06-05 10:23:30 -0700

    asdf

--- Fx.Core.Tests/System/Linq/V2/V2EnumerableUnitTests.cs
+++ Fx.Core.Tests/System/Linq/V2/V2EnumerableUnitTests.cs
@@ -919,9 +919,6 @@ namespace System.Linq.V2
             }
         }
 
-        //// TODO start a design document
-        //// TODO document that you are not implementing thenby and thenbydescending yet because they are externally extensible and maybe benefit from a monad of their own
-        //// TODO remind yourself that you could just use default interface implementations for all of linq; however, this doesn't establish the pattern for others to add their own extension methods and introduce their own interfaces; further, there can be conflicts between the interface and existing concrete collection implementations (or such things could be introduced in the future); those conflicts can cause compiler errors when upgrading linq versions in certain cases
         //// TODO implement empty; is emptydefault still relevant?
         //// TODO implement range; is rangedeafult still relevant?
         //// TODO implement repeat; is repeatdeafult still relevant?
