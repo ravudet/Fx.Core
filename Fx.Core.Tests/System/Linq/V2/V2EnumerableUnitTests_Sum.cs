e2e55de5ab2536dd1d5a551b7d33ffe3e5aa966a
ravudet <170215607+ravudet@users.noreply.github.com>
2024-06-06 09:50:03 -0700

    adding sumints unit tests

---
+++ Fx.Core.Tests/System/Linq/V2/V2EnumerableUnitTests_Sum.cs
@@ -0,0 +1,267 @@
+namespace System.Linq.V2
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public sealed partial class V2EnumerableUnitTests
+    {
+        [TestMethod]
+        public void SumIntsMixinWithOverload()
+        {
+            var enumerable = new MockSumIntsMixinWithOverload().AsV2Enumerable();
+            var sumed = enumerable.Sum(element => element.GetHashCode());
+            Assert.AreEqual(MockSumIntsMixinWithOverload.Result, sumed);
+        }
+
+        private sealed class MockSumIntsMixinWithOverload : ISumableMixin<object>
+        {
+            public static int Result { get; } = new object().GetHashCode();
+
+            public int Sum(Func<object, int> selector)
+            {
+                return Result;
+            }
+
+            public IEnumerator<object> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public void SumIntsMixinWithoutOverloadAndMonadWhereSourceIsMixin()
+        {
+            var enumerable = new MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsMixin().AsV2Enumerable();
+            var sumed = enumerable.Sum(element => element.GetHashCode());
+            Assert.AreEqual(MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsMixin.Result, sumed);
+        }
+
+        private sealed class MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsMixin : ISumableMixin<object>, IEnumerableMonad<object>
+        {
+            public static int Result { get; } = new object().GetHashCode();
+
+            private static class ResultMonadFactory<T>
+            {
+                private static Unit<T> factory;
+
+                static ResultMonadFactory()
+                {
+                    ResultMonad<T>.SetFactory();
+                    if (factory == null)
+                    {
+                        throw new NotSupportedException("The SetFactory method did not set the factory");
+                    }
+                }
+
+                public static Unit<T> Factory
+                {
+                    get
+                    {
+                        return factory;
+                    }
+                    set
+                    {
+                        if (factory != null)
+                        {
+                            throw new NotSupportedException("Cannot set the factory after initialization");
+                        }
+
+                        factory = value;
+                    }
+                }
+            }
+
+            public IV2Enumerable<object> Source { get; } = SourceEnumerable.Instance;
+
+            private sealed class SourceEnumerable : ISumableMixin<object>
+            {
+                private SourceEnumerable()
+                {
+                }
+
+                public static SourceEnumerable Instance { get; } = new SourceEnumerable();
+
+                public int Sum(Func<object, int> selector)
+                {
+                    return MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsMixin.Result;
+                }
+
+                public IEnumerator<object> GetEnumerator()
+                {
+                    throw new NotImplementedException();
+                }
+
+                IEnumerator IEnumerable.GetEnumerator()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public Unit<TSource> Unit<TSource>()
+            {
+                return ResultMonadFactory<TSource>.Factory;
+            }
+
+            public sealed class ResultMonad<T> : IEnumerableMonad<T>
+            {
+                public static void SetFactory()
+                {
+                    ResultMonadFactory<T>.Factory = (IV2Enumerable<T> source) => new ResultMonad<T>(source);
+                }
+
+                private ResultMonad(IV2Enumerable<T> source)
+                {
+                    this.Source = source;
+                }
+
+                public IV2Enumerable<T> Source { get; }
+
+                public Unit<TSource> Unit<TSource>()
+                {
+                    return ResultMonadFactory<TSource>.Factory;
+                }
+
+                public IEnumerator<T> GetEnumerator()
+                {
+                    throw new NotImplementedException();
+                }
+
+                IEnumerator IEnumerable.GetEnumerator()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public IEnumerator<object> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public void SumIntsMixinWithoutOverloadAndMonadWhereSourceIsNotMixin()
+        {
+            var enumerable = new MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsNotMixin().AsV2Enumerable();
+            var sumed = enumerable.Sum(element => element.GetHashCode());
+            Assert.AreEqual(MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsNotMixin.Element.GetHashCode(), sumed);
+        }
+
+        private sealed class MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsNotMixin : ISumableMixin<object>, IEnumerableMonad<object>
+        {
+            public static object Element { get; } = new object();
+
+            private static class ResultMonadFactory<T>
+            {
+                private static Unit<T> factory;
+
+                static ResultMonadFactory()
+                {
+                    ResultMonad<T>.SetFactory();
+                    if (factory == null)
+                    {
+                        throw new NotSupportedException("The SetFactory method did not set the factory");
+                    }
+                }
+
+                public static Unit<T> Factory
+                {
+                    get
+                    {
+                        return factory;
+                    }
+                    set
+                    {
+                        if (factory != null)
+                        {
+                            throw new NotSupportedException("Cannot set the factory after initialization");
+                        }
+
+                        factory = value;
+                    }
+                }
+            }
+
+            public IV2Enumerable<object> Source { get; } = new[] { MockSumIntsMixinWithoutOverloadAndMonadWhereSourceIsNotMixin.Element }.ToV2Enumerable();
+
+            public Unit<TSource> Unit<TSource>()
+            {
+                return ResultMonadFactory<TSource>.Factory;
+            }
+
+            public sealed class ResultMonad<T> : IEnumerableMonad<T>
+            {
+                public static void SetFactory()
+                {
+                    ResultMonadFactory<T>.Factory = (IV2Enumerable<T> source) => new ResultMonad<T>(source);
+                }
+
+                private ResultMonad(IV2Enumerable<T> source)
+                {
+                    this.Source = source;
+                }
+
+                public IV2Enumerable<T> Source { get; }
+
+                public Unit<TSource> Unit<TSource>()
+                {
+                    return ResultMonadFactory<TSource>.Factory;
+                }
+
+                public IEnumerator<T> GetEnumerator()
+                {
+                    throw new NotImplementedException();
+                }
+
+                IEnumerator IEnumerable.GetEnumerator()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public IEnumerator<object> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public void SumIntsMixinWithoutOverloadAndNoMonad()
+        {
+            var enumerable = new MockSumIntsMixinWithoutOverloadAndNoMonad().AsV2Enumerable();
+            var sumed = enumerable.Sum(element => element.GetHashCode());
+            Assert.AreEqual(MockSumIntsMixinWithoutOverloadAndNoMonad.Element.GetHashCode(), sumed);
+        }
+
+        private sealed class MockSumIntsMixinWithoutOverloadAndNoMonad : ISumableMixin<object>
+        {
+            public static object Element { get; } = new object();
+
+            public IEnumerator<object> GetEnumerator()
+            {
+                yield return Element;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
