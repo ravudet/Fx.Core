cd64562987d20c1ecd0655fba1f9237b7bca6050
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-18 15:42:28 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -9,5 +9,16 @@
             Assert.That.IsTrue(true);
             Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(false));
         }
+
+        [TestMethod]
+        public void IsTrueNullable()
+        {
+            bool? condition = null;
+            Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition));
+            condition = true;
+            Assert.That.IsTrue(condition);
+            condition = false;
+            Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition));
+        }
     }
 }
