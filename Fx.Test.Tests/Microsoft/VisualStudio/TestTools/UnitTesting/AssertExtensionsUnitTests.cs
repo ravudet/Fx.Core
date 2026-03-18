e2db5e32fb00ae9ed202618215ac5be2c4b7eefe
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-18 15:45:43 -0700

    asfd

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -6,8 +6,11 @@
         [TestMethod]
         public void IsTrue()
         {
-            Assert.That.IsTrue(true);
-            Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(false));
+            bool condition = true;
+            Assert.That.IsTrue(condition);
+
+            condition = false;
+            Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition));
         }
 
         [TestMethod]
@@ -15,10 +18,41 @@
         {
             bool? condition = null;
             Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition));
+
             condition = true;
             Assert.That.IsTrue(condition);
+
             condition = false;
             Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition));
+        }
+
+        [TestMethod]
+        public void IsTrueMessage()
+        {
+            var message = "some message";
+            bool condition = true;
+            Assert.That.IsTrue(condition, message);
+
+            condition = false;
+            var exception = Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition, message));
+            Assert.IsTrue(exception.Message.Contains(message));
+        }
+
+        [TestMethod]
+        public void IsTrueNullableMessage()
+        {
+            var message = "some message";
+            bool? condition = null;
+            var exception = Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition, message));
+            Assert.IsTrue(exception.Message.Contains(message));
+
+            condition = true;
+            Assert.That.IsTrue(condition);
+
+            condition = false;
+            message = "another message";
+            exception = Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(condition, message));
+            Assert.IsTrue(exception.Message.Contains(message));
         }
     }
 }
