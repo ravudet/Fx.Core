d35925ceeb2dc0083910d2850cae6c83d7001c3f
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-18 15:37:04 -0700

    adding tests for the new extensions

---
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    [TestClass]
+    public sealed class AssertExtensionsUnitTests
+    {
+        [TestMethod]
+        public void IsTrue()
+        {
+            Assert.That.IsTrue(true);
+            Assert.ThrowsException<AssertFailedException>(() => Assert.That.IsTrue(false));
+        }
+    }
+}
