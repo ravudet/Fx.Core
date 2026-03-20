3af12bf9797be61fcdcef1cc7c5366d57398dc75
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-20 07:52:23 -0700

    asdf

--- Fx.Test/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensions.cs
+++ Fx.Test/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensions.cs
@@ -7,6 +7,11 @@
 
     public static class AssertExtensions
     {
+        public static void Foo()
+        {
+        }
+
+
         /// <param name="assert">The <see cref="Assert"/> instance that is being extended</param>
         /// <inheritdoc cref="Assert.IsTrue(bool)"/>
         public static void IsTrue(this Assert assert, [DoesNotReturnIf(false)] bool condition)
