caa9861d81b4c71b205464fd992bb768d56a3e47
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-24 07:22:14 -0700

    asdf

--- Analyzer1/Analyzer1/Analyzer1Analyzer.cs
+++ Analyzer1/Analyzer1/Analyzer1Analyzer.cs
@@ -84,8 +84,22 @@
 
                 var typeInfo = context.SemanticModel.GetTypeInfo(identifierSyntax);
 
+                var type = typeInfo.Type;
+
+                if (type.Name.Equals("Assert"))
+                {
+                    var assemblyIdentity = type.ContainingAssembly.Identity;
+                    if (assemblyIdentity.Name.Equals("Microsoft.VisualStudio.TestPlatform.TestFramework") &&
+                        assemblyIdentity.HasPublicKey &&
+                        ////assemblyIdentity.PublicKeyToken.SequenceEqual(StringToByteArrayFastest("b03f5f7f11d50a3a"))
+                        assemblyIdentity.PublicKey.SequenceEqual(StringToByteArrayFastest("002400000480000094000000060200000024000052534131000400000100010007D1FA57C4AED9F0A32E84AA0FAEFD0DE9E8FD6AEC8F87FB03766C834C99921EB23BE79AD9D5DCC1DD9AD236132102900B723CF980957FC4E177108FC607774F29E8320E92EA05ECE4E821C0A5EFE8F1645C4C0C93C1AB99285D622CAA652C1DFAD63D745D6F2DE5F17E5EAF0FC4963D261C8A12436518206DC093344D5AD293"))
+                        )
+                    {
+                    }
+                }
 
 
+                //// 002400000480000094000000060200000024000052534131000400000100010007D1FA57C4AED9F0A32E84AA0FAEFD0DE9E8FD6AEC8F87FB03766C834C99921EB23BE79AD9D5DCC1DD9AD236132102900B723CF980957FC4E177108FC607774F29E8320E92EA05ECE4E821C0A5EFE8F1645C4C0C93C1AB99285D622CAA652C1DFAD63D745D6F2DE5F17E5EAF0FC4963D261C8A12436518206DC093344D5AD293
 
                 ////context.SemanticModel.Compilation.ReferencedAssemblyNames.First()
 
@@ -98,6 +112,32 @@
                 //// TODO if they don't depend on unit testing, there is no error
                 //// TODO if they don't have fx.test, add it as a nuget package
             }
+        }
+
+        public static byte[] StringToByteArrayFastest(string hex)
+        {
+            if (hex.Length % 2 == 1)
+                throw new Exception("The binary key cannot have an odd number of digits");
+
+            byte[] arr = new byte[hex.Length >> 1];
+
+            for (int i = 0; i < hex.Length >> 1; ++i)
+            {
+                arr[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+            }
+
+            return arr;
+        }
+
+        public static int GetHexVal(char hex)
+        {
+            int val = (int)hex;
+            //For uppercase A-F letters:
+            //return val - (val < 58 ? 48 : 55);
+            //For lowercase a-f letters:
+            //return val - (val < 58 ? 48 : 87);
+            //Or the two combined, but a bit slower:
+            return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
         }
     }
 }
