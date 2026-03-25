f82a3db28e41d44d1e3936a9ed26f713e49ac7d2
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-25 05:44:23 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -6,13 +6,22 @@
     public sealed class AssertExtensionsUnitTests
     {
         //// TODO you are at identation preferences and you are re-ordering the config file to follow the ui; make sure there are no duplicates
-        
+
+        //// TODO i'm not sure i'm going to like `csharp_new_line_before_open_brace = all`; things like anonymous types and object/array initializers are sometimes small enough that inline works better, and having to use a suppression to get inline will ruin the look
+
+
+
+
+
+
+
+
         //// TODO i'm not convinced that the UI is a canonical list
-        
+
 
 
         //// TODO add custom roslyn analayzer for `assert.that`
-        
+
 
 
 
