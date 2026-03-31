81e16e7ad6ce507d288408c7a0b8abb01fdc237c
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-31 14:59:38 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -57,6 +57,11 @@
 
 
 
+        public interface ITest
+        {
+            void Test();
+        }
+
         public record Point(int X, int Y);
         public record Segment(Point Start, Point End);
 
