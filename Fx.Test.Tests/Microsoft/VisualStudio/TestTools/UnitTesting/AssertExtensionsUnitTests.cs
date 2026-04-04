fc6bdcd72ca35f4b81927dab5636d0fc62c4a6c0
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-04 06:04:22 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -20,7 +20,8 @@
         //// TODO https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1031 the first example says `systemexception` but should say `system.exception`
 
         //// TODO when you go back through to write the articles, consider the severity of the rules; some of them should likely be `suggestion` or `silent`
-        //// TODO "suggestion" really means "message", but anything becomes noise when you don't suppress them; would a good rule be to suppress messages into the suppression file? (so that you don't reduce readability)
+        //// TODO "suggestion" really means "message", but anything becomes noise when you don't suppress them; would a good SOP be to suppress messages into the suppression file? (so that you don't reduce readability)
+        //// TODO should you have a test project for your config file?
 
 
 
