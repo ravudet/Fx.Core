0411a29ed8655a1ab6e98205d48acaf46b753ce9
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-19 05:40:09 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -200,10 +200,12 @@ class C {
 
 
 
+
         //// TODO dotnet_diagnostic.CA1000.severity = warning doesn't flag the method call like the doc says
         //// TODO https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1031 the first example says `systemexception` but should say `system.exception`
         //// TODO broken xml: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca5393
 
+        //// TODO distributing via nuget: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/configuration-files#distribution-in-nuget-packages
         //// TODO note early on in the article series that you are big on "don't just enable something because it's a rule; you need to understand it"; anything else is superstition
         //// TODO when you go back through to write the articles, consider the severity of the rules; some of them should likely be `suggestion` or `silent`
         //// TODO "suggestion" really means "message", but anything becomes noise when you don't suppress them; would a good SOP be to suppress messages into the suppression file? (so that you don't reduce readability)
@@ -344,6 +346,11 @@ class C {
             {
 
             }
+
+
+
+            int value = 3;
+            Console.Write(value);
         }
 
 
