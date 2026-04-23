a5476945d88bfed3eb265fe06824c5cfcce3848b
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-23 05:44:29 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -207,7 +207,6 @@ class C {
         //// TODO ide0007 and ide0008 links both "appear" as ide0008 (compare with ide0003 and ide0009; compare with ide0020 and ide0038) in the list on the left https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/style-rules/ide0007-ide0008
         //// TODO couldn't get this to trigger: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/style-rules/ide0016
         //// TODO ide0023 and ide0024 both "appear" as ide0024 (compare with ide0003 and ide0009; compare with ide0020 and ide0038); https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/style-rules/ide0023-ide0024
-        //// TODO couldn't get this to trigger: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/style-rules/ide0023-ide0024
 
         //// TODO distributing via nuget: https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/configuration-files#distribution-in-nuget-packages
         //// TODO note early on in the article series that you are big on "don't just enable something because it's a rule; you need to understand it"; anything else is superstition
