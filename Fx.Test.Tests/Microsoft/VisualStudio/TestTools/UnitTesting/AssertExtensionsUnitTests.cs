21cbdfe5de4cbc3bd512700534cc2f8b82b96cda
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-04 14:37:23 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -329,7 +329,6 @@ class C {
 
         //// TODO add custom roslyn analayzer for `assert.that`
         ////    TODO allow unsafe is an example of a codefix that updates the project
-        //// TODO add an analyzer that *disallows* primary constructors (the opposite of csharp_style_prefer_primary_constructors)
         //// TODO you know, the old style cop docs used to actually have pretty good justifications for why you want to change your code...
         ////    TODO csharp_style_prefer_implicitly_typed_lambda_expression also triggers cases where you are passing a lambda as a parameter to a method `Frub((int x) => { });`
         ////    TODO i think `csharp_style_inlined_variable_declaration` would make a lot more sense if `while (!int.TryParse(value, out var parsed)) { } Console.WriteLine(parsed);` worked
@@ -344,9 +343,23 @@ class C {
 
 
 
+        class Primary(int i)
+        {
+            private readonly int i = i;
+
+            public void Foo()
+            {
+            }
+
+        }
+
+
+
         [TestMethod]
         public void ImplicitCast()
         {
+            var primary = new Primary(1);
+
             var data = new object[] { 1, "asdf" };
             foreach (int datum in data)
             {
