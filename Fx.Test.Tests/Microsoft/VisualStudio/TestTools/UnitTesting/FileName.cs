6da72b5d53fb455ff5f92085f52c503f7f9c272d
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-10 06:28:18 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/FileName.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/FileName.cs
@@ -3,13 +3,16 @@
 namespace Microsoft.VisualStudio.TestTools.UnitTesting
 {
     using System;
-
     using Foo;
 
     internal class FileName
     {
         public static void DoWork()
         {
+            if(true)
+            {
+            }
+
             Demo.DoWork();
             Console.WriteLine("asdF");
         }
