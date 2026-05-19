86ee2b66b5dc68339591884a1f609d16a9aa21f2
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-19 05:46:43 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -368,7 +368,7 @@ class C {
 
 
 
-        //// TODO add custom roslyn analayzer for `assert.that`
+        //// TODO add custom Roslyn analayzer for `assert.that`
         ////    TODO allow unsafe is an example of a codefix that updates the project
         //// TODO you know, the old style cop docs used to actually have pretty good justifications for why you want to change your code...
         ////    TODO i think `csharp_style_inlined_variable_declaration` would make a lot more sense if `while (!int.TryParse(value, out var parsed)) { } Console.WriteLine(parsed);` worked
