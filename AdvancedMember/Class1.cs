eca9c2af76d8b41f2c9381fe602259976bc5f4bd
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-27 07:22:06 -0700

    asdf

---
+++ AdvancedMember/Class1.cs
@@ -0,0 +1,14 @@
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace AdvancedMember
+{
+    using System.ComponentModel;
+
+    public static class Class1
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void DoWork()
+        {
+        }
+    }
+}
