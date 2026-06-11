7432f5492e0a721b36ee50c370b6b940bfa52882
ravudet <170215607+ravudet@users.noreply.github.com>
2026-06-11 07:40:16 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/FileName.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/FileName.cs
@@ -4,22 +4,11 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting
 {
     using System;
     using System.Collections.Generic;
-    using System.Collections.Immutable;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
-    using System.Reflection;
-    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
     using Foo;
-
-    using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.Diagnostics;
-    using Microsoft.CodeAnalysis.MSBuild;
-    using Microsoft.CodeAnalysis.Text;
 
     internal class FileName
     {
