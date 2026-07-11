50a2ee8c8192180ed66daf1445f24a9fc2fe44c9
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-11 10:24:23 -0700

    asdf

---
+++ source/Fx.Core.Tests/System/Threading/Tasks/TaskExtensionsUnitTests.cs
@@ -0,0 +1,27 @@
+﻿namespace System.Threading.Tasks
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class TaskExtensionsUnitTests
+    {
+        [TestMethod]
+        public async Task ToTaskWrapper()
+        {
+            var expectedValue = 42;
+            var value = await TaskExtensionsUnitTests.ToTaskWrapperHelper(expectedValue);
+
+            Assert.AreEqual(expectedValue, value);
+        }
+
+        private static TaskWrapper<int> ToTaskWrapperHelper(int value)
+        {
+            return ToTaskWrapperHelperImpl(value).ToTaskWrapper();
+        }
+
+        private static async Task<int> ToTaskWrapperHelperImpl(int value)
+        {
+            return await Task.FromResult(value);
+        }
+    }
+}
