fe06eabfaf00efe3dc58df35c5639dfb14895198
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-12 05:41:27 -0700

    asdf

--- Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
+++ Fx.Test.Tests/Microsoft/VisualStudio/TestTools/UnitTesting/AssertExtensionsUnitTests.cs
@@ -378,6 +378,7 @@ class C {
 
         //// TODO use the "tip" from here: https://learn.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/tutorials/how-to-write-csharp-analyzer-code-fix#explore-the-analyzer-template to reset the mef components
 
+        //// TODO do you want interaction libraries to use underscores?
         //// TODO get all of the nuget stuff in fx.core.analyzers correct
         //// TODO put analyzer stuff in the correct folder
         //// TODO make fx.core publish a nuget package
