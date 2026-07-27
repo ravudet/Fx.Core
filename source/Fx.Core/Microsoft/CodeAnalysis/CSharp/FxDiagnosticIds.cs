66cb061252efae1fa29dcbc895b3b35371df489d
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-27 06:11:26 -0700

    asdf

--- source/Fx.Core/Microsoft/CodeAnalysis/CSharp/FxDiagnosticIds.cs
+++ source/Fx.Core/Microsoft/CodeAnalysis/CSharp/FxDiagnosticIds.cs
@@ -2,7 +2,7 @@
 {
     public static class FxDiagnosticIds
     {
-        private const string IdPrefix = "FX"; //// TODO apparently it's not good to use 2-character prefixes
+        private const string IdPrefix = "FX"; // it is suggested not to use two-character prefixes [here](https://learn.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/choosing-diagnostic-ids#considerations), but that's our name, so i am just going with it
 
         public const string BaseLowercaseTypeNameAnalyzerDiagnosticId = $"{FxDiagnosticIds.IdPrefix}0001";
     }
