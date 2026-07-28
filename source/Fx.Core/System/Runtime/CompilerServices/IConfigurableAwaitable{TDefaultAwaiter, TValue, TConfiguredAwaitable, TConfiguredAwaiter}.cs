53629c2d01ed49318749a2a4ab617d5648b5d52b
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-28 07:28:49 -0700

    asdf

--- source/Fx.Core/System/Runtime/CompilerServices/IConfigurableAwaitable{TDefaultAwaiter, TValue, TConfiguredAwaitable, TConfiguredAwaiter}.cs
+++ source/Fx.Core/System/Runtime/CompilerServices/IConfigurableAwaitable{TDefaultAwaiter, TValue, TConfiguredAwaitable, TConfiguredAwaiter}.cs
@@ -3,8 +3,19 @@
     public interface IConfigurableAwaitable<out TDefaultAwaiter, out TValue, out TConfiguredAwaitable, out TConfiguredAwaiter> : IAwaitable<TDefaultAwaiter, TValue>
         where TDefaultAwaiter : IAwaiter<TValue>
 #if NET6_0_OR_GREATER
+        , allows ref struct
+#endif
+#if NET6_0_OR_GREATER
+        where TValue : allows ref struct
+#endif
         where TConfiguredAwaitable : IAwaitable<TConfiguredAwaiter, TValue>
+#if NET6_0_OR_GREATER
+        , allows ref struct
+#endif
         where TConfiguredAwaiter : IAwaiter<TValue>
+#if NET6_0_OR_GREATER
+        , allows ref struct
+#endif
     {
         TConfiguredAwaitable ConfigureAwait(bool continueOnCapturedContext); //// TODO use the enum and have an extension for the bool instead
     }
