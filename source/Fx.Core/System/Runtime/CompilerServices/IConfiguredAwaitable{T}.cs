8bf3225d321d78bb4e71a5a066c29de9d0797226
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-28 07:30:43 -0700

    asdf

--- source/Fx.Core/System/Runtime/CompilerServices/IConfiguredAwaitable{T}.cs
+++ source/Fx.Core/System/Runtime/CompilerServices/IConfiguredAwaitable{T}.cs
@@ -1,6 +1,6 @@
 ﻿namespace System.Runtime.CompilerServices
 {
-    public interface IConfiguredAwaitable<out T> : IConfigurableAwaitable<IAwaiter<T>, T, IAwaitable<T>, IAwaiter<T>>
+    public interface IConfigurableAwaitable<out T> : IConfigurableAwaitable<IAwaiter<T>, T, IAwaitable<T>, IAwaiter<T>>
 #if NET6_0_OR_GREATER
         where T : allows ref struct
 #endif
