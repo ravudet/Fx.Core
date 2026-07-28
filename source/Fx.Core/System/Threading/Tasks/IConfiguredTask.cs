5522283d9eb9aff1deee25b982da33bce8099b15
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-28 07:38:13 -0700

    asdf

---
+++ source/Fx.Core/System/Threading/Tasks/IConfiguredTask.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Threading.Tasks
+{
+    using System.Runtime.CompilerServices;
+
+    public interface IConfiguredTask<out T> : IAwaitable<T>
+#if NET6_0_OR_GREATER
+        where T : allows ref struct
+#endif
+    {
+        Exception? Exception { get; }
+
+        bool IsCanceled { get; }
+    }
+}
--- source/Fx.Core/System/Threading/Tasks/ITask.cs
+++ source/Fx.Core/System/Threading/Tasks/ITask.cs
@@ -11,14 +11,4 @@
 
         bool IsCanceled { get; }
     }
-
-    public interface IConfiguredTask<out T> : IAwaitable<T>
-#if NET6_0_OR_GREATER
-        where T : allows ref struct
-#endif
-    {
-        Exception? Exception { get; }
-
-        bool IsCanceled { get; }
-    }
 }
