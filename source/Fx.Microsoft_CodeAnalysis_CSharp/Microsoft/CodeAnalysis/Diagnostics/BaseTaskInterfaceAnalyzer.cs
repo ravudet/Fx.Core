6351332d2de509d27cef5d0e28a323269ef47519
ravudet <170215607+ravudet@users.noreply.github.com>
2026-07-29 07:49:16 -0700

    Asdf

--- source/Fx.Microsoft_CodeAnalysis_CSharp/Microsoft/CodeAnalysis/Diagnostics/BaseTaskInterfaceAnalyzer.cs
+++ source/Fx.Microsoft_CodeAnalysis_CSharp/Microsoft/CodeAnalysis/Diagnostics/BaseTaskInterfaceAnalyzer.cs
@@ -34,25 +34,22 @@
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            var config = context.Options.AnalyzerConfigOptionsProvider.GetOptions(context.Compilation.SyntaxTrees.First());
-            var configResult = config.TryGetValue("ravudet", out var configValue);
+            /*var config = context.Options.AnalyzerConfigOptionsProvider.GetOptions(context.Compilation.SyntaxTrees.First());
+            var configResult = config.TryGetValue("ravudet", out var configValue);*/
 
             //// TODO you should also find cases like async funcs
-            //// TODO you still need a codefix for this
 
 
             var myType = context.Compilation.GetTypeByMetadataName("System.Threading.Tasks.ITask"); //// TODO i don't know how you're supposed to get the generic
             var message = string.Empty;
             if (myType is null)
             {
-                // message = "The 'Fx.Core' package which contains 'ITask<T>' is also missing.";
+                // message = "The 'Fx.Core' package (which contains 'ITask<T>') is also missing.";
             }
 
 
