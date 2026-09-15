e0e6cb16ceee54e355b9a8062ffd8a186e9f277f
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-15 15:30:39 -0700

    asdf

--- source/Fx.Analyzers.GlobalConfig.Tests/Fx/GlobalConfigTests.cs
+++ source/Fx.Analyzers.GlobalConfig.Tests/Fx/GlobalConfigTests.cs
@@ -69,7 +69,6 @@
             var test = await GetTestString().ConfigureAwait(false); //// TODO do you like this variable name?
             var editorConfig = await GetEditorConfigString().ConfigureAwait(false); //// TODO do you like this variable name?
 
-            //// TODO add a teste for lowercasetypename
             //// TODO move below helper types into their own project
             //// TODO remove any unnecessary dependencies
             //// TODO can you rename the Content folder to `_resources`?
