d1a3c1a85194f7a74bf0c7433e9fb6d0c2bad9a7
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-15 14:58:40 -0700

    asdf

---
+++ source/Fx.Analyzers.GlobalConfig.Tests/Content/FX0001.cs
@@ -0,0 +1,11 @@
+[assembly: System.CLSCompliant(true)]
+[assembly: System.Reflection.AssemblyVersion("1.0.0")]
+
+namespace FX0001
+{
+    using System.Threading.Tasks;
+
+    public static class Foo
+    {
+    }
+}
