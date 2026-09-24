533f8d76c88b0582f573d26bea2627992cd6b2a3
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 08:12:10 -0700

    asdf

---
+++ source/Fx.Microsoft_CodeAnalysis_NetAnalyzers/Microsoft/CodeAnalysis/Diagnostics/DiagnosticAnalyzersExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Microsoft.CodeAnalysis.Diagnostics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class DiagnosticAnalyzersExtensions
+    {
+        public static IEnumerable<DiagnosticAnalyzer> Default(this DiagnosticAnalyzers _) //// TODO make this a property
+        {
+            /*var foo = Path.GetDirectoryName(typeof(Microsoft.NetFramework.Analyzers.TypesShouldNotExtendCertainBaseTypesAnalyzer).Assembly.Location);
+            foo = Path.Combine(foo, "Microsoft.CodeAnalysis.CSharp.CodeStyle.dll");*/
+
+            var paths = new[]
+            {
+                typeof(Microsoft.NetFramework.Analyzers.TypesShouldNotExtendCertainBaseTypesAnalyzer).Assembly.Location,
+                //foo,
+                ////typeof(Microsoft.CodeAnalysis.Completion.CompletionTags).Assembly.Location,
+                //typeof(Microsoft.CodeAnalysis.Diagnostics.AnalysisContext).Assembly.Location, // TODO foo depends on this
+                ////@"C:\github\OddTrotter\Fx.Core\Fx.Test.Tests\Microsoft.CodeAnalysis.NetAnalyzers.dll",
+                ////@"C:\github\OddTrotter\Fx.Core\Fx.Test.Tests\Microsoft.CodeAnalysis.Analyzers.dll",
+                ////@"C:\github\OddTrotter\Fx.Core\Fx.Test.Tests\Microsoft.CodeAnalysis.CSharp.Analyzers.dll",
+                ////@"C:\github\OddTrotter\Fx.Core\Fx.Test.Tests\Microsoft.CodeAnalysis.CSharp.dll",
+            };
+
+            ////var assembly = Assembly.Load(@"C:\github\OddTrotter\Fx.Core\Fx.Test.Tests\Microsoft.CodeAnalysis.NetAnalyzers.dll");
+
+            /*var type = typeof(Microsoft.CodeAnalysis.Analyzers.MetaAnalyzers.DiagnosticAnalyzerAttributeAnalyzer);
+            var another = (DiagnosticAnalyzer)Activator.CreateInstance(type)!;*/
+
+            return paths.SelectMany(path => DiagnosticAnalyzers.Extensions.Load(path))/*.Append(another).ToImmutableArray()*/;
+        }
+    }
+}
