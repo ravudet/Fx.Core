88fa06a3146304d3b5ff5cb90cc4c872a429aa8b
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 12:56:54 -0700

    asdf

---
+++ source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing+MSTest_TestFramework/Microsoft/CodeAnalysis/Testing/ExceptionUtilities.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.CodeAnalysis.Testing
+{
+    using System;
+
+    internal static class ExceptionUtilities
+    {
+        public static Exception Unreachable => new InvalidOperationException("This program location is thought to be unreachable.");
+    }
+}
--- source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing+MSTest_TestFramework/Microsoft/CodeAnalysis/Testing/MSTestVerifier.cs
+++ source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing+MSTest_TestFramework/Microsoft/CodeAnalysis/Testing/MSTestVerifier.cs
@@ -153,10 +153,4 @@
             }
         }
     }
-
-
-    internal static class ExceptionUtilities
-    {
-        public static Exception Unreachable => new InvalidOperationException("This program location is thought to be unreachable.");
-    }
 }
