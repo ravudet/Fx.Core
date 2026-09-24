1da670de89827737e8476cd50539bc0f425b1bfd
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 12:46:33 -0700

    asdf

--- source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing/Microsoft/CodeAnalysis/Testing/MSTestVerifier.cs
+++ source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing/Microsoft/CodeAnalysis/Testing/MSTestVerifier.cs
@@ -6,6 +6,8 @@
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
 
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     //// TODO this was pulled from an existing package, but you couldn't get the versions working correctly; see if you actually need this class (and, by extension, this project) at all
 
     public class MSTestVerifier : IVerifier
