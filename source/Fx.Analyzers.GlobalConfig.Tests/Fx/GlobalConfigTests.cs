dc56cdf31ac4ac47b8868282986e6ea20344f9f0
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 13:02:07 -0700

    asdf

--- source/Fx.Analyzers.GlobalConfig.Tests/Fx/GlobalConfigTests.cs
+++ source/Fx.Analyzers.GlobalConfig.Tests/Fx/GlobalConfigTests.cs
@@ -118,47 +118,7 @@
         }
     }
 
-    public class CsharpAnalyzerTest<TVerifier> : BaseAnalyzerTest<TVerifier>
-        where TVerifier : IVerifier, new()
-    {
-        [SetsRequiredMembers]
-        public CsharpAnalyzerTest()
-        {
-            //// TODO put setting default analyzers in base type
+    
 
-            base.CompilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true);
-            base.ParseOptions = new CSharpParseOptions(LanguageVersion.Default, DocumentationMode.Diagnose);
-            base.DiagnosticAnalyzers = GlobalConfigTests.DefaultAnalyzers;
-        }
-
-        public override string Language { get; } = LanguageNames.CSharp;
-
-        protected override string DefaultFileExt { get; } = "cs";
-    }
-
-    public abstract class BaseAnalyzerTest<TVerifier> : AnalyzerTest<TVerifier>
-        where TVerifier : IVerifier, new()
-    {
-        public required CompilationOptions CompilationOptions { private get; init; }
-
-        public required ParseOptions ParseOptions { private get; init; }
-
-        public required IEnumerable<DiagnosticAnalyzer> DiagnosticAnalyzers { private get; init; }
-
-        protected override CompilationOptions CreateCompilationOptions()
-        {
-            return this.CompilationOptions;
-        }
-
-        protected override ParseOptions CreateParseOptions()
-        {
-            return this.ParseOptions;
-        }
-
-        protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers()
-        {
-            return this.DiagnosticAnalyzers;
-        }
-
-    }
+    
 }
---
+++ source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing/Microsoft/CodeAnalysis/Testing/CsharpAnalyzerTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.CodeAnalysis.Testing
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Microsoft.CodeAnalysis.CSharp;
+
+    public class CsharpAnalyzerTest<TVerifier> : BaseAnalyzerTest<TVerifier>
+        where TVerifier : IVerifier, new()
+    {
+        [SetsRequiredMembers]
+        public CsharpAnalyzerTest()
+        {
+            //// TODO put setting default analyzers in base type
+
+            base.CompilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true);
+            base.ParseOptions = new CSharpParseOptions(LanguageVersion.Default, DocumentationMode.Diagnose);
+            base.DiagnosticAnalyzers = GlobalConfigTests.DefaultAnalyzers;
+        }
+
+        public override string Language { get; } = LanguageNames.CSharp;
+
+        protected override string DefaultFileExt { get; } = "cs";
+    }
+}
