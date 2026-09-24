42f7117e63b99be7627c5750cac6e762a3975b5d
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 08:30:49 -0700

    asdf

---
+++ source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing/Microsoft/CodeAnalysis/Testing/MSTestVerifier.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.CodeAnalysis.Testing
+{
+    //// TODO this was pulled from an existing package, but you couldn't get the versions working correctly; see if you actually need this class (and, by extension, this project) at all
+
+    internal class MSTestVerifier
+    {
+    }
+}
