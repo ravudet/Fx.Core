aed42de668e1e4c0fef3a17fd2700d8216678951
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 13:11:55 -0700

    asdf

--- source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing/Microsoft/CodeAnalysis/Testing/CsharpAnalyzerTest.cs
+++ source/Fx.Microsoft_CodeAnalysis_Analyzer_Testing/Microsoft/CodeAnalysis/Testing/CsharpAnalyzerTest.cs
@@ -1,8 +1,10 @@
 ﻿namespace Microsoft.CodeAnalysis.Testing
 {
+    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
 
     using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
 
     public class CsharpAnalyzerTest<TVerifier> : BaseAnalyzerTest<TVerifier>
         where TVerifier : IVerifier, new()
@@ -14,11 +16,38 @@
 
             base.CompilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true);
             base.ParseOptions = new CSharpParseOptions(LanguageVersion.Default, DocumentationMode.Diagnose);
-            base.DiagnosticAnalyzers = GlobalConfigTests.DefaultAnalyzers;
+            //// TODO base.DiagnosticAnalyzers = GlobalConfigTests.DefaultAnalyzers;
+            
+            var test = new FakeAnalyzerTest()
+            {
+                TestState = new SolutionState(string.Empty, string.Empty, string.Empty, string.Empty),
+            }
         }
 
         public override string Language { get; } = LanguageNames.CSharp;
 
         protected override string DefaultFileExt { get; } = "cs";
+
+        private sealed class FakeAnalyzerTest : AnalyzerTest<TVerifier>
+        {
+            public override string Language { get; }
+
+            protected override string DefaultFileExt { get; }
+
+            protected override CompilationOptions CreateCompilationOptions()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            protected override ParseOptions CreateParseOptions()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
     }
 }
