e0c11291eb25b9c142926a8859299957a72b3a22
ravudet <170215607+ravudet@users.noreply.github.com>
2026-09-24 08:14:19 -0700

    asdf

--- source/Fx.Core/Microsoft/CodeAnalysis/Diagnostics/DiagnosticAnalyzers.cs
+++ source/Fx.Core/Microsoft/CodeAnalysis/Diagnostics/DiagnosticAnalyzers.cs
@@ -1,6 +1,6 @@
 ﻿namespace Microsoft.CodeAnalysis.Diagnostics
 {
-    public sealed class DiagnosticAnalyzers //// TODO i like this pattern for "static extension methods", but i don't know that the property name "extensions" is the best...
+    public sealed class DiagnosticAnalyzers //// TODO i like this pattern for "static extension methods", but i don't know that the property name "extensions" is the best... //// TODO you could maybe call it "static" or something?
     {
         private DiagnosticAnalyzers()
         {
